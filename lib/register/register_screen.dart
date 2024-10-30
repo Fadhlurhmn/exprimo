4742bf957fb7819bc128716d1700607d00df1751
DeaPutri17 <putrinastitidea@gmail.com>
2024-10-30 09:55:09 +0700

    connect register to database

--- lib/register/register_screen.dart
+++ lib/register/register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:exprimo/login/background.dart';
 import 'package:exprimo/constants.dart';
@@ -13,10 +14,11 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController emailController = TextEditingController();
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController = TextEditingController();
-  final _formKey = GlobalKey<FormState>(); // Membuat GlobalKey untuk form
+  final _formKey = GlobalKey<FormState>();
+  final DatabaseReference usersRef = FirebaseDatabase.instance.ref("users");
 
   @override
   Widget build(BuildContext context) {
@@ -26,137 +28,41 @@ class _RegisterScreenState extends State<RegisterScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            // Background Widget
             Background(isLoginActive: false),
-
-            // Kotak berwarna secondaryColor di bagian bawah
             Expanded(
               child: Container(
                 color: secondaryColor,
                 padding: EdgeInsets.symmetric(horizontal: size.width * 0.06),
                 child: SingleChildScrollView(
-                  child: Form( // Menggunakan Form untuk validasi
+                  child: Form(
                     key: _formKey,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        SizedBox(height: size.height * 0.05), // Jarak atas
-                        Container(
-                          width: double.infinity,
-                          child: TextFormField(
-                            controller: usernameController,
-                            decoration: InputDecoration(
-                              hintText: 'Masukkan username',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                              filled: true,
-                              fillColor: Colors.white,
-                              prefixIcon: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Image.asset("assets/icons/Person.png"),
-                              ),
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Silakan masukkan username.';
-                              }
-                              return null;
-                            },
-                          ),
+                        SizedBox(height: size.height * 0.05),
+                        _buildTextField(
+                          controller: emailController,
+                          hintText: 'Masukkan email',
+                          icon: "assets/icons/Email.png",
+                          validatorMessage: 'Silakan masukkan email.',
                         ),
                         SizedBox(height: size.height * 0.01),
-                        Container(
-                          width: double.infinity,
-                          child: TextFormField(
-                            controller: passwordController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              hintText: 'Masukkan password',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                              filled: true,
-                              fillColor: Colors.white,
-                              prefixIcon: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Image.asset("assets/icons/Lock.png"),
-                              ),
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Silakan masukkan password.';
-                              }
-                              return null;
-                            },
-                          ),
+                        _buildTextField(
+                          controller: usernameController,
+                          hintText: 'Masukkan username',
+                          icon: "assets/icons/Person.png",
+                          validatorMessage: 'Silakan masukkan username.',
                         ),
                         SizedBox(height: size.height * 0.01),
-                        Container(
-                          width: double.infinity,
-                          child: TextFormField(
-                            controller: confirmPasswordController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              hintText: 'Konfirmasi password',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                              filled: true,
-                              fillColor: Colors.white,
-                              prefixIcon: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Image.asset("assets/icons/Lock.png"),
-                              ),
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Silakan konfirmasi password.';
-                              } else if (value != passwordController.text) {
-                                return 'Password tidak cocok.';
-                              }
-                              return null;
-                            },
-                          ),
+                        _buildTextField(
+                          controller: passwordController,
+                          hintText: 'Masukkan password',
+                          icon: "assets/icons/Lock.png",
+                          isPassword: true,
+                          validatorMessage: 'Silakan masukkan password.',
                         ),
                         SizedBox(height: size.height * 0.05),
-
-                        // Tombol Register di bagian bawah
-                        Container(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                // Jika validasi berhasil, navigasi ke homepage
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => Homepage()),
-                                );
-                              }
-                              // Tidak perlu menampilkan pop-up jika validasi gagal, karena 
-                              // pesan kesalahan sudah ditampilkan di bawah TextFormField
-                            },
-                            child: Text(
-                              'Register',
-                              style: TextStyle(
-                                color: secondaryColor,
-                                fontSize: 20,
-                                fontFamily: 'Nunito',
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: primaryColor,
-                              padding: EdgeInsets.symmetric(vertical: 20),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                            ),
-                          ),
-                        ),
+                        _buildRegisterButton(context),
                         SizedBox(height: size.height * 0.03),
                       ],
                     ),
@@ -170,11 +76,92 @@ class _RegisterScreenState extends State<RegisterScreen> {
     );
   }
 
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String hintText,
+    required String icon,
+    required String validatorMessage,
+    bool isPassword = false,
+    String? Function(String?)? additionalValidator,
+  }) {
+    return Container(
+      width: double.infinity,
+      child: TextFormField(
+        controller: controller,
+        obscureText: isPassword,
+        decoration: InputDecoration(
+          hintText: hintText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          filled: true,
+          fillColor: Colors.white,
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(icon),
+          ),
+        ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return validatorMessage;
+          }
+          if (additionalValidator != null) {
+            return additionalValidator(value);
+          }
+          return null;
+        },
+      ),
+    );
+  }
+
+  Widget _buildRegisterButton(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () async {
+          if (_formKey.currentState!.validate()) {
+            // Simpan data user ke Firebase Realtime Database
+            await usersRef.push().set({
+              "email": emailController.text,
+              "username": usernameController.text,
+              "password": passwordController.text,
+            }).then((_) {
+              print("User berhasil ditambahkan!");
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Homepage()),
+              );
+            }).catchError((error) {
+              print("Gagal menambahkan user: $error");
+            });
+          }
+        },
+        child: Text(
+          'Register',
+          style: TextStyle(
+            color: secondaryColor,
+            fontSize: 20,
+            fontFamily: 'Nunito',
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: primaryColor,
+          padding: EdgeInsets.symmetric(vertical: 20),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   void dispose() {
+    emailController.dispose();
     usernameController.dispose();
     passwordController.dispose();
-    confirmPasswordController.dispose();
     super.dispose();
   }
 }
